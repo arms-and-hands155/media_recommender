c134d07dc7f0fb353c9bb2aebac53b5317b460a1
arms-and-hands155 <armandnik16@Gmail.com>
2026-07-29 16:05:50 -0400

    collecting more data

--- src/data_collection.py
+++ src/data_collection.py
@@ -4,7 +4,8 @@ import json
 import os
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-DATA_PATH = os.path.join(SCRIPT_DIR, "..", "data", "data.jsonl")
+ANIME_DATA_PATH = os.path.join(SCRIPT_DIR, "..", "data", "anime_data.jsonl")
+MANGA_DATA_PATH = os.path.join(SCRIPT_DIR, "..", "data", "manga_data.jsonl")
 
 ANILIST_URL = "https://graphql.anilist.co" #Getting data from AniList API
 
@@ -32,27 +33,46 @@ def fetch_page(page, media_type="ANIME"):
     response = requests.post(ANILIST_URL, json={"query": QUERY, "variables": variables})
     response.raise_for_status()
     return response.json()["data"]["Page"]
-i=1
+i=100
 data = []
-
+#Get Anime data
 while True:
     x=fetch_page(i)
     data.extend(x['media'])
     
-    if i == 100:
-        with open(DATA_PATH, 'a') as file:
+    if i == 100: #API caps at 5000 requests 
+        with open(ANIME_DATA_PATH, 'a') as file:
             for item in data:
                 file.write(f'{json.dumps(item)}\n')
         break
     i+=1
     
     if i % 10 == 0:
-        with open(DATA_PATH, 'a') as file:
+        with open(ANIME_DATA_PATH, 'a') as file:
             for item in data:
                 file.write(f'{json.dumps(item)}\n')
         data.clear()
         
-    time.sleep(3) #Ensures we wont have too many requests too quick
+    time.sleep(2) #Ensures we wont have too many requests too quick
 
-print("DID IT")
-
+data.clear()
+i=1
+#Get Manga data
+while True:
+    x=fetch_page(i, media_type="MANGA")
+    data.extend(x['media'])
+    
+    if i == 100: 
+        with open(MANGA_DATA_PATH, 'a') as file:
+            for item in data:
+                file.write(f'{json.dumps(item)}\n')
+        break
+    i+=1
+    
+    if i % 10 == 0:
+        with open(MANGA_DATA_PATH, 'a') as file:
+            for item in data:
+                file.write(f'{json.dumps(item)}\n')
+        data.clear()
+        
+    time.sleep(2) 
